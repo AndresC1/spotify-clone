b67e405188e730ea1ac794b3c299152e81526dd5
AndresC1 <andrescornejolira@gmail.com>
2023-01-25 13:00:55 -0600

    Instalaciòn de tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'tailwindcss/tailwind.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <h1>Hola mundo</h1>
+  <h1 className='w-full h-20 bg-[#333] text-[#eee] font-bold text-3xl flex justify-center items-center'>Hola mundo</h1>
 )
