9a94648298f4e0074164493f40c5be0eb7d19b24
AndresC1 <andrescornejolira@gmail.com>
2023-01-29 00:27:55 -0600

    creacion de plantilla base

---
+++ src/page/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+import Nabvar from '../components/Navbar/Nabvar'
+import Reproductor from '../components/Reproductor/Reproductor';
+
+
+export default function HomePage() {
+  return (
+    <div className='flex justify-start items-stretch h-[100vh]'>
+        <Nabvar/>
+        <div className='w-full'>
+          <Outlet/>
+        </div>
+        <Reproductor/>
+    </div>
+  )
+}
