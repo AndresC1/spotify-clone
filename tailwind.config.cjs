8c0143b163b1cbd419f60bc0c0aa6312b3f5a8f5
AndresC1 <andrescornejolira@gmail.com>
2023-01-29 00:40:57 -0600

    Creacion de la animacion de muestra de cancion

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,22 @@ module.exports = {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Color-Fondo': '#2B2B2B',
+        'Color-Spotify': '#1ED760'
+      },
+      animation: {
+        'scroll-content': 'animation-scroll 10s linear infinite'
+      },
+      keyframes: {
+        'animation-scroll': {
+          '0%': { transform:  'translateX(0%)' },
+          '9%': { transform:  'translateX(0%)' },
+          '100%': { transform:  'translateX(-100%)' }
+        }
+      }
+    },
   },
   plugins: [],
 }
