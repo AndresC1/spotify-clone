e1a473869b011b45ec6aad3777eca70cb275f5ba
AndresC1 <andrescornejolira@gmail.com>
2023-01-31 13:05:20 -0600

    Titulo de secciones de datos

---
+++ src/components/section/titleSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TitleSection({titulo}) {
+  return (
+    <h1 className='text-[#F5F5F5] text-lg font-bold w-full py-4 px-10'>
+        {titulo}
+    </h1>
+  )
+}
