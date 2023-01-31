d005857618d567ce5b1f4dd64e11c42a39666b0d
AndresC1 <andrescornejolira@gmail.com>
2023-01-31 13:05:46 -0600

    Caja de carga de los datos

---
+++ src/components/section/LoadBox.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function LoadBox() {
+  return (
+    <div className='lg:w-44 w-36 h-52 bg-slate-800 rounded-xl animate-pulse'>
+        <div className='w-10/12 m-auto my-4 h-32 bg-slate-700 rounded-lg'></div>
+        <div className='w-10/12 m-auto h-4 bg-slate-700 rounded-lg'></div>
+    </div>
+  )
+}
